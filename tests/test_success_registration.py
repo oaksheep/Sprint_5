2645732c63ce0dfb3d769793c4aafb2068336c00
Lidiia Dubovetc <dubovetslida@gmail.com>
2024-04-29 13:23:21 +0200

    sprint 5

--- tests/test_success_registration.py
+++ tests/test_success_registration.py
@@ -1,17 +1,19 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from locators import StellarBurgersLocators
-from data import StellarBurgersTestData
+from faker import Faker
 import urls
 
+
+fake = Faker()
 class TestStellarBurgersRegistration:
 
     def test_success_registration(self, driver):
         driver.find_element(*StellarBurgersLocators.ENTER_ACCOUNT_BUTTON).click()
         driver.find_element(*StellarBurgersLocators.REGISTRATION_BUTTON).click()
-        driver.find_element(*StellarBurgersLocators.REGISTRATION_NAME_INPUT).send_keys(StellarBurgersTestData.NAME)
-        driver.find_element(*StellarBurgersLocators.REGISTRATION_EMAIL_INPUT).send_keys(StellarBurgersTestData.REGISTRATION_EMAIL)
-        driver.find_element(*StellarBurgersLocators.REGISTRATION_PASSWORD_INPUT).send_keys(StellarBurgersTestData.PASSWORD)
+        driver.find_element(*StellarBurgersLocators.REGISTRATION_NAME_INPUT).send_keys(fake.name())
+        driver.find_element(*StellarBurgersLocators.REGISTRATION_EMAIL_INPUT).send_keys(fake.email())
+        driver.find_element(*StellarBurgersLocators.REGISTRATION_PASSWORD_INPUT).send_keys(fake.password())
         driver.find_element(*StellarBurgersLocators.CONFIRM_REGISTRATION_BUTTON).click()
         WebDriverWait(driver, 5).until(
             expected_conditions.visibility_of_element_located
